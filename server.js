711cbec0a2519bc6c340ed9cf854ca2031547665
ali naqvi <alinaqvi@ali.local>
2025-07-21 12:57:41 +0530

    Added Phase 2 backend updates

--- server.js
+++ server.js
@@ -18,5 +18,9 @@ app.get("/", (req, res) => {
   });
   
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
