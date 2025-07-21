46cc2c6ac0edce714af07135f17b8103fdbd795a
ali naqvi <alinaqvi@ali.local>
2025-07-21 12:40:30 +0530

    Added Phase 2 backend updates

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
+app.get("/", (req, res) => {
+    res.send("Backend is live!");
+  });
+  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
